382daf08cafbfbdde8cbe9ae136a53696d256756
Novazeb <novaberkatsyukurzebua@gmail.com>
2026-02-26 21:00:03 +0700

    Fix mixed content error HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- 1. Wajib tambahkan ini di atas
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Tambahkan 3 baris ini untuk memaksa HTTPS di server Railway
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
